e0a63f5f8deec37973413e0d249112ec8bd151a9
Jan <jan.gladine@uantwerpen.student.be>
2023-03-06 08:54:08 +0100

    Added TCP server socket

--- main/Main.java
+++ main/Main.java
@@ -1,7 +1,15 @@
 package main;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("hello world");
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5000);
+        Socket socket = serverSocket.accept();
+        InputStream input = socket.getInputStream();
     }
 }
